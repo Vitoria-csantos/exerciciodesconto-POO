0b95e92252c5ae8569c9a1ff63160821d5cabbdb
Vitoria-csantos <vitoria.santos659@etec.sp.gov.br>
2024-04-08 12:11:36 -0300

    Adicionando o exercício. Resolver erros depois.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace exerciciodesconto_POO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltexto = new System.Windows.Forms.Label();
+            this.txtpreco = new System.Windows.Forms.TextBox();
+            this.lblcodigo = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.lbldesconto = new System.Windows.Forms.Label();
+            this.txtdesconto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbltexto
+            // 
+            this.lbltexto.AutoSize = true;
+            this.lbltexto.Location = new System.Drawing.Point(176, 32);
+            this.lbltexto.Name = "lbltexto";
+            this.lbltexto.Size = new System.Drawing.Size(76, 13);
+            this.lbltexto.TabIndex = 0;
+            this.lbltexto.Text = "Digite o preço:";
+            // 
+            // txtpreco
+            // 
+            this.txtpreco.Location = new System.Drawing.Point(167, 57);
+            this.txtpreco.Name = "txtpreco";
+            this.txtpreco.Size = new System.Drawing.Size(100, 20);
+            this.txtpreco.TabIndex = 1;
+            // 
+            // lblcodigo
+            // 
+            this.lblcodigo.AutoSize = true;
+            this.lblcodigo.Location = new System.Drawing.Point(405, 32);
+            this.lblcodigo.Name = "lblcodigo";
+            this.lblcodigo.Size = new System.Drawing.Size(81, 13);
+            this.lblcodigo.TabIndex = 2;
+            this.lblcodigo.Text = "Digite o código:";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(397, 57);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtcodigo.TabIndex = 3;
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Location = new System.Drawing.Point(298, 106);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(75, 23);
+            this.btnenviar.TabIndex = 4;
+            this.btnenviar.Text = "Enviar";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // lbldesconto
+            // 
+            this.lbldesconto.AutoSize = true;
+            this.lbldesconto.Location = new System.Drawing.Point(292, 166);
+            this.lbldesconto.Name = "lbldesconto";
+            this.lbldesconto.Size = new System.Drawing.Size(100, 13);
+            this.lbldesconto.TabIndex = 5;
+            this.lbldesconto.Text = "Seu desconto é de:";
+            // 
+            // txtdesconto
+            // 
+            this.txtdesconto.Location = new System.Drawing.Point(292, 196);
+            this.txtdesconto.Name = "txtdesconto";
+            this.txtdesconto.Size = new System.Drawing.Size(100, 20);
+            this.txtdesconto.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtdesconto);
+            this.Controls.Add(this.lbldesconto);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.lblcodigo);
+            this.Controls.Add(this.txtpreco);
+            this.Controls.Add(this.lbltexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltexto;
+        private System.Windows.Forms.TextBox txtpreco;
+        private System.Windows.Forms.Label lblcodigo;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label lbldesconto;
+        private System.Windows.Forms.TextBox txtdesconto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exerciciodesconto_POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            double num1,codigo , result ;
+            num1 = Convert.ToDouble(txtpreco.Text);
+           
+            if(codigo = "norte")
+            {
+                (5 /num1) * 100;
+                txtdesconto.Text == result.ToString();
+            }
+            else
+            {
+                if(codigo = "sul")
+                {
+                    (15 / num1) * 100;
+                    txtdesconto.Text == result.ToString();
+                }
+                else
+                {
+                    if (codigo = "sudeste")
+                    {
+                        (7 / num1) * 100;
+                        txtdesconto.Text == result.ToString();
+                    }
+                    else
+                    {
+                        if(codigo = "nordeste")
+                        {
+                            (12 / num1) * 100;
+                            txtdesconto.Text == result.ToString();
+                        }
+                        else
+                        {
+                            if(codigo = "centro-oeste")
+                            {
+                                (20 / num1) * 100;
+                                txtdesconto.Text == result.ToString();
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                    }
+                }
+                
+
+            }
+        }
+    }
+}
